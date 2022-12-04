8cab4aff559a15c9abd22c3e8027797ab4744f42
Anton Zubkov <zub0296@mail.ru>
2022-12-04 19:42:28 +0500

    Created project

---
+++ ByteConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+
+namespace DeepArchive
+{
+    public sealed class ByteConverter
+    {
+        #region Methods
+
+        public byte[] GetBytes(string value)
+        {
+            return Encoding.ASCII.GetBytes(value);
+        }
+
+        public void PrintBytes(byte[] bytes)
+        {
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                Console.WriteLine(bytes[i]);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DeepArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ByteConverter byteConverter = new ByteConverter();
+            byteConverter.PrintBytes(byteConverter.GetBytes("Hello World!"));
+        }
+    }
+}
